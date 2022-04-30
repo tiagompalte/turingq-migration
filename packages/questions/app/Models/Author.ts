29a4bf337be709546da450acf0b321a5fb84df33
Tiago M Palte <tiagompalte@hotmail.com>
2022-04-30 11:37:13 -0500

    remove subscriptions inside questions package

--- packages/questions/app/Models/Author.ts
+++ packages/questions/app/Models/Author.ts
@@ -2,7 +2,6 @@ import { column, hasMany, HasMany, BaseModel } from '@ioc:Adonis/Lucid/Orm'
 
 import Answer from 'App/Models/Answer'
 import Question from 'App/Models/Question'
-import Subscription from './Subscription'
 
 export default class Author extends BaseModel {
   // Requerido pelo AdonisJS para permitir que usemos UUID
@@ -23,7 +22,4 @@ export default class Author extends BaseModel {
 
   @hasMany(() => Answer, { foreignKey: 'authorId' })
   public answers: HasMany<typeof Answer>
-
-  @hasMany(() => Subscription, { foreignKey: 'userId' })
-  public subscriptions: HasMany<typeof Subscription>
 }
